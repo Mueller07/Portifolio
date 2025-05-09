18fb87d0ac9c73f2ae4eacacd11eb44bfdb4b2a7
Isabela Mueller <162368740+Mueller07@users.noreply.github.com>
2025-05-09 14:43:06 -0300

    Delete conteudo directory

--- conteudo/script.js
+++
@@ -1,8 +0,0 @@
-function validaBuscar(){
-    if(document.querySelector('#inputlupa').value == ''){
-        alert('Insira um valor para buscar');
-    return false;
-}
-}
-let barraBusca = document.querySelector('#form-busca').onsubmit = validaBuscar;
- 
